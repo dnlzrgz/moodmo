62c0023082264d2c46613bcb97d51271dbdc031d
dnlzrgz <24715931+dnlzrgz@users.noreply.github.com>
2023-11-21 12:48:22 +0100

    test(pages): add test for home page

---
+++ pages/tests.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+from django.urls import reverse
+
+
+class HomepageTests(TestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_available_by_name(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_template_name_correct(self):
+        response = self.client.get(reverse("home"))
+        self.assertTemplateUsed(response, "home.html")
