10dfd8155342496e399cd6eaecd5a9f6009fb3ca
dnlzrgz <24715931+dnlzrgz@users.noreply.github.com>
2023-11-23 12:34:59 +0100

    test(moods): add tests for form

---
+++ moods/tests/test_forms.py
@@ -0,0 +1,47 @@
+from datetime import datetime
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from moods.forms import MoodForm
+
+
+class MoodFormTest(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username="tester",
+            email="test@test.com",
+            password="test1234",
+        )
+
+    def test_valid_form(self):
+        data = {
+            "mood": 1,
+            "timestamp": datetime.utcnow(),
+        }
+        form = MoodForm(data)
+
+        self.assertTrue(form.is_valid())
+
+    def test_invalid_form(self):
+        data = {
+            "timestamp": datetime.utcnow(),
+        }
+        form = MoodForm(data)
+
+        self.assertFalse(form.is_valid())
+
+    def test_form_save(self):
+        data = {
+            "mood": 1,
+            "note_title": "Testing",
+            "note": "Test",
+            "timestamp": datetime.utcnow(),
+        }
+        form = MoodForm(data=data)
+        self.assertTrue(form.is_valid())
+
+        form.instance.user = self.user
+        mood = form.save()
+
+        self.assertEqual(mood.mood, 1)
+        self.assertEqual(mood.note_title, "Testing")
+        self.assertEqual(mood.note, "Test")
