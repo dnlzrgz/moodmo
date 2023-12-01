f9ef6b92e1d6acc21ddae7c727452747ae64b4ec
dnlzrgz <24715931+dnlzrgz@users.noreply.github.com>
2023-12-01 13:30:40 +0100

    feat(templ): add title section to home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./templates/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': {
+          '50': '#f0fdfc',
+          '100': '#ccfbf7',
+          '200': '#99f6ee',
+          '300': '#5eeade',
+          '400': '#2dd4c5',
+          '500': '#14b8a9',
+          '600': '#0d9488',
+          '700': '#0f766d',
+          '800': '#115e57',
+          '900': '#134e49',
+          '950': '#042f2b',
+        },
+      },
+    },
   },
   plugins: [],
 };
