97344d873a698f039865d016a900866b472c8dda
dnlzrgz <24715931+dnlzrgz@users.noreply.github.com>
2023-12-18 19:07:25 +0100

    fix(docker): solve issue with no styles in prod Dockerfile

--- moodmo/wsgi.py
+++ moodmo/wsgi.py
@@ -1,16 +1,17 @@
 """
 WSGI config for moodmo project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
+from whitenoise import WhiteNoise
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'moodmo.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "moodmo.settings")
 
 application = get_wsgi_application()
+application = WhiteNoise(application)
+application.add_files(
+    "/app/staticfiles",
+    prefix="staticfiles/",
+)
