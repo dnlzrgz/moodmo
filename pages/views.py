ae154bf37ee82d6acc64fc2c08558bf83afdc83e
dnlzrgz <24715931+dnlzrgz@users.noreply.github.com>
2024-03-04 12:01:47 +0100

    feat(settings): add support for activities on import

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,6 @@
 import csv
 import json
+from django.db import transaction
 from django.conf import settings
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse, JsonResponse
@@ -13,6 +14,7 @@ from pages.forms import (
     ExportOptionsForm,
 )
 from moods.models import Mood
+from activities.models import Activity
 
 
 class HomePageView(TemplateView):
@@ -42,11 +44,10 @@ class ImportView(LoginRequiredMixin, FormView):
     form_class = UploadFileForm
     template_name = "pages/import.html"
     success_url = reverse_lazy("mood_list")
-    max_upload_size = 2621440
+    max_upload_size = 2621440  # 2.5MB
 
     def post(self, request, *args, **kwargs):
-        form_class = self.get_form_class()
-        form = self.get_form(form_class)
+        form = self.get_form()
         if form.is_valid():
             return self.form_valid(form)
         else:
@@ -55,54 +56,77 @@ class ImportView(LoginRequiredMixin, FormView):
     def form_valid(self, form):
         file = form.cleaned_data["file"]
 
-        # Check that file is a CSV or a JSON
-        if not (file.name.endswith(".csv") or file.name.endswith(".json")):
-            form.add_error("file", "Only CSV or JSON files are allowed.")
+        if not (
+            file.name.endswith(".csv") or file.name.endswith(".json")
+        ) or file.content_type not in ["text/csv", "application/json"]:
+            form.add_error("file", "Only CSV or JSON files are supported.")
             return self.form_invalid(form)
 
         if file.size > self.max_upload_size:
             form.add_error(
                 "file",
-                f"Please keep filesize under {self.max_upload_size}. Current filesize {file.size}",
+                "The file is too big. The maximum support filesize is 2.5 MB.",
             )
+            return self.form_invalid(form)
 
         try:
             if file.name.endswith(".csv"):
-                csv_data = file.read().decode("utf-8")
-                csv_reader = csv.DictReader(csv_data.splitlines())
-                moods = [
-                    Mood(
-                        user=self.request.user,
-                        mood=row["mood"],
-                        note_title=row["note_title"],
-                        note=row["note"],
-                        timestamp=row["timestamp"],
-                    )
-                    for row in csv_reader
-                ]
-
-                Mood.objects.bulk_create(moods)
-
+                self.import_csv(file)
             elif file.name.endswith(".json"):
-                json_data = json.loads(file.read().decode("utf-8"))
-                moods = [
-                    Mood(
-                        user=self.request.user,
-                        mood=item["mood"],
-                        note_title=item["note_title"],
-                        note=item["note"],
-                        timestamp=item["timestamp"],
-                    )
-                    for item in json_data
-                ]
-
-                Mood.objects.bulk_create(moods)
-
-        except (csv.Error, json.JSONDecodeError) as e:
-            form.add_error("file", f"Error reading file: {e}")
+                self.import_json(file)
+        except (csv.Error, json.JSONDecodeError, KeyError, ValueError) as e:
+            form.add_error("file", f"Error reading or parsing file: {e}")
             return self.form_invalid(form)
 
         return super().form_valid(form)
+
+    def import_csv(self, file):
+        with transaction.atomic():
+            csv_data = file.read().decode("utf-8")
+            csv_reader = csv.DictReader(csv_data.splitlines())
+            for row in csv_reader:
+                mood = Mood.objects.create(
+                    user=self.request.user,
+                    mood=row["mood"],
+                    note_title=row["note_title"],
+                    note=row["note"],
+                    timestamp=timezone.datetime.strptime(
+                        row["timestamp"],
+                        "%Y-%m-%d %H:%M:%S",
+                    ),
+                )
+
+                activities = row["activities"].split(", ")
+                for name in activities:
+                    activity, _ = Activity.objects.get_or_create(
+                        user=self.request.user,
+                        name=name,
+                    )
+
+                    mood.activities.add(activity)
+
+    def import_json(self, file):
+        with transaction.atomic():
+            json_data = json.loads(file.read().decode("utf-8"))
+            for item in json_data:
+                mood = Mood.objects.create(
+                    user=self.request.user,
+                    mood=item["mood"],
+                    note_title=item["note_title"],
+                    note=item["note"],
+                    timestamp=timezone.datetime.strptime(
+                        item["timestamp"], "%Y-%m-%d %H:%M:%S"
+                    ),
+                )
+
+                activities = item.get("activities", [])
+                for name in activities:
+                    activity, _ = Activity.objects.get_or_create(
+                        user=self.request.user,
+                        name=name,
+                    )
+
+                    mood.activities.add(activity)
 
 
 class ExportView(LoginRequiredMixin, FormView):
