02c511b1d6df627653d670f29999ac7270d54a9f
dnlzrgz <24715931+dnlzrgz@users.noreply.github.com>
2024-04-04 22:04:59 +0200

    test(pages): add tests for statistics and settings pages

--- pages/tests.py
+++ pages/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse
+from utils.testing import create_fake_user
 
 
 class HomePageTests(TestCase):
@@ -14,6 +15,42 @@ class HomePageTests(TestCase):
     def test_template_name_correct(self):
         response = self.client.get(reverse("home"))
         self.assertTemplateUsed(response, "pages/home.html")
+
+
+class SettingsPageTests(TestCase):
+    def setUp(self):
+        self.credentials, self.user = create_fake_user()
+        self.url = reverse("settings")
+
+    def test_authenticated_user_can_access_view(self):
+        self.client.login(**self.credentials)
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "pages/settings.html")
+
+    def test_not_authenticated_user_cannot_access_view(self):
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, 302)
+
+
+class StatisticsPageTests(TestCase):
+    def setUp(self):
+        self.credentials, self.user = create_fake_user()
+        self.url = reverse("statistics")
+
+    def test_authenticated_user_can_access_view(self):
+        self.client.login(**self.credentials)
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "pages/statistics.html")
+
+    def test_not_authenticated_user_cannot_access_view(self):
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, 302)
 
 
 class RobotsTxtTests(TestCase):
